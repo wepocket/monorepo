6078dad48bae4a97ca0db251c437aac10ed3126e
Fernando Lopez <axnotliztac@gmail.com>
2025-07-14 16:59:56 -0600

    fix pass key 2

--- packages/app/src/app/api/passkey/route.ts
+++ packages/app/src/app/api/passkey/route.ts
@@ -8,87 +8,101 @@ const prisma = new PrismaClient()
 export const POST = async (req: Request) => {
   const { type, registration, authentication } = await req.json()
 
-  const userId = await getUserCookie()
-  const credentialExists = await prisma.passkey.findUnique({
-    where: {
-      userId,
-    },
-  })
+  try {
+    const userId = await getUserCookie()
+    const credentialExists = await prisma.passkey.findUnique({
+      where: {
+        userId,
+      },
+    })
 
-  if (type === 'challenge') {
-    const challenge = server.randomChallenge()
+    if (type === 'challenge') {
+      await prisma.passkey.delete({
+        where: {
+          userId,
+        },
+      })
 
-    if (Boolean(credentialExists?.credential)) {
+      const challenge = server.randomChallenge()
+
+      if (Boolean(credentialExists?.credential)) {
+        await prisma.passkey.update({
+          where: {
+            userId,
+          },
+          data: {
+            challenge,
+          },
+        })
+      } else {
+        await prisma.passkey.create({
+          data: {
+            challenge,
+            userId,
+          },
+        })
+      }
+
+      return Response.json({
+        success: true,
+        challenge,
+        isRegistered: Boolean(credentialExists?.credential),
+        credential: credentialExists?.credential && JSON.parse(credentialExists?.credential as string),
+      })
+    }
+
+    if (type === 'verifyRegistration') {
+      const expected = {
+        challenge: credentialExists?.challenge || '',
+        origin: process.env.APP_HOST || 'http://localhost:3000',
+      }
+      const registrationParsed = await server.verifyRegistration(registration, expected)
+
       await prisma.passkey.update({
         where: {
           userId,
         },
         data: {
-          challenge,
+          credential: JSON.stringify(registrationParsed.credential),
         },
       })
-    } else {
-      await prisma.passkey.create({
-        data: {
-          challenge,
-          userId,
-        },
+
+      return Response.json({
+        success: true,
+        registrationParsed,
       })
     }
 
-    return Response.json({
-      success: true,
-      challenge,
-      isRegistered: Boolean(credentialExists?.credential),
-      credential: credentialExists?.credential && JSON.parse(credentialExists?.credential as string),
-    })
-  }
+    if (type === 'verifyAuthentication') {
+      const expected = {
+        challenge: credentialExists?.challenge || '',
+        origin: process.env.APP_HOST || 'http://localhost:3000',
+        userVerified: true,
+        counter: -1,
+      }
 
-  if (type === 'verifyRegistration') {
-    const expected = {
-      challenge: credentialExists?.challenge || '',
-      origin: process.env.APP_HOST || 'http://localhost:3000',
+      const verified = await server.verifyAuthentication(
+        authentication,
+        JSON.parse(credentialExists?.credential as string) as unknown as CredentialInfo,
+        expected
+      )
+
+      return Response.json({
+        success: true,
+        verified,
+      })
     }
-    const registrationParsed = await server.verifyRegistration(registration, expected)
+  } catch (_e) {
+    const e = _e as Error
 
-    // await prisma.passkey.update({
-    //   where: {
-    //     userId,
-    //   },
-    //   data: {
-    //     credential: JSON.stringify(registrationParsed.credential),
-    //   },
-    // })
+    console.log(e.message)
 
-    return Response.json({
-      success: true,
-      registrationParsed,
-    })
-  }
-
-  if (type === 'verifyAuthentication') {
-    const expected = {
-      challenge: credentialExists?.challenge || '',
-      origin: process.env.APP_HOST || 'http://localhost:3000',
-      userVerified: true,
-      counter: -1,
-    }
-
-    const verified = await server.verifyAuthentication(
-      authentication,
-      JSON.parse(credentialExists?.credential as string) as unknown as CredentialInfo,
-      expected
-    )
-
-    return Response.json({
-      success: true,
-      verified,
-    })
+    return Response.json({ success: false }, { status: 500 })
   }
 }
 
 export const GET = async () => {
-  console.log(await prisma.passkey.deleteMany())
+  console.log(await prisma.passkey.findMany())
 
   return Response.json({ success: true })
 }
